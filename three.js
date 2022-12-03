066c464eafc521948e363c327cdafc9ac46aaa94
emlala <emmi.lahtisalo@accenture.com>
2022-12-03 14:42:56 +0200

    bit of restructuring

--- three.js
+++ three.js
@@ -3,19 +3,24 @@ import fs from 'fs';
 
 const text = fs.readFileSync("./data/input3.txt").toString('utf-8').split("\n");
 
+// return numeric value of letter
+function letterValue(letter) {
+  if (letter == letter.toUpperCase()) {
+    return letter.charCodeAt(0) - 38; // A-Z values 27-52
+  } else {
+    return letter.charCodeAt(0) - 96; // a-z values 1-26
+  }
+}
+
 // part 1
-// see if there's a matching letter in the first and second half of each string
+// find the matching letter in the first and second half of each string
 let sum1 = 0;
 
 function findMatch1(compartment1, compartment2) {
   for (let i = 0; i < compartment1.length; i++) {
     let letter = compartment1[i];
     if (compartment2.includes(letter)) {
-      if (letter == letter.toUpperCase()) {
-        sum1 += letter.charCodeAt(0) - 38; // A-Z values 27-52
-      } else {
-        sum1 += letter.charCodeAt(0) - 96; // a-z values 1-26
-      }
+      sum1 += letterValue(letter);
       break;
     }
   }
@@ -31,31 +36,22 @@ for (let i = 0; i < text.length; i++) {
 console.log(`Sum 1: ${sum1}`);
 
 // part 2
-// see if there's a matching letter in each of the three strings
+// find the matching letter in each of the three strings
 let sum2 = 0;
 
 function findMatch2(rucksack1, rucksack2, rucksack3) {
   for (let i = 0; i < rucksack1.length; i++) {
     let letter = rucksack1[i];
     if (rucksack2.includes(letter) && rucksack3.includes(letter)) {
-      if (letter == letter.toUpperCase()) {
-        sum2 += letter.charCodeAt(0) - 38; // A-Z values 27-52
-      } else {
-        sum2 += letter.charCodeAt(0) - 96; // a-z values 1-26
-      }
+      sum2 += letterValue(letter);
       break;
     }
   }
 }
 
-let data = [];
 for (let i = 0; i < text.length; i += 3) {
-  const chunk = text.slice(i, i + 3);
-  data.push(chunk);
-}
-
-for (let i = 0; i < data.length; i++) {
-  findMatch2(data[i][0], data[i][1], data[i][2]);
+  let chunk = text.slice(i, i + 3);
+  findMatch2(chunk[0], chunk[1], chunk[2]);
 }
 
 console.log(`Sum 2: ${sum2}`);
