20c869e48c208b370b06d3570ade32b6e9566225
emlala <emmi.lahtisalo@accenture.com>
2022-12-10 14:41:30 +0200

    day 10: commenting and merging 1 and 2 a bit

--- day10.js
+++ day10.js
@@ -1,76 +1,68 @@
 'use strict';
 import fs from 'fs';
 
-// turn data into array
 const data = fs.readFileSync("./data/input10.txt").toString('utf-8').trim().split("\n");
 
-// Part 1
-
-let cycle1 = 0;
-let x1 = 1;
+let cycle = 0;
+let x = 1;
 let signalStrength = [];
+let pixel = 0;
+let crt = [[], [], [], [], [], []];
+let crtRow = 0;
+let sprite = [x - 1 , x, x + 1];
 
-function cycle() {
-  cycle1++;
-  if (cycle1 % 40 === 20) {
-    signalStrength.push(cycle1 * x1);
+// Part 1
+// at cycles 20, 60, 100, 140, 180 and 220, calculate signal strength
+// signal strength = cycle * x
+function CalSigStr() {
+  cycle++;
+  if (cycle % 40 === 20) {
+    signalStrength.push(cycle * x);
   }
 }
-
-for (let i = 0; i < data.length; i++) {
-  data[i] = data[i].split(" ");
-  data[i][1] = parseInt(data[i][1]);
-
-  if (data[i][0] === "noop") {
-    cycle();
-  } else if (data[i][0] === "addx") {
-    cycle();
-    cycle();
-    x1 += data[i][1];
-  }
-}
-
-let sum = signalStrength.reduce((a, b) => a + b, 0);
-console.log("Part 1: " + sum);
 
 // Part 2
 // "draw" the crt message, each cycle is one pixel in the 40*6 crt
 // x marks the centre position of the 3-pixel sprite
 // if the current pixel overlaps with the sprite, draw a #, otherwise draw a .
-
-let cycle2 = 0;
-let x2 = 1;
-let crt = [[], [], [], [], [], []];
-let crtRow = 0;
-let sprite = [0, 1, 2];
-
 function draw() {
   // switch row(/array) when cycle reaches 40 and start counting from 0 again
-  if (cycle2 === 40) {
+  if (pixel === 40) {
     crtRow++;
-    cycle2 = 0;
+    pixel = 0;
   }
 
-  if (sprite.includes(cycle2)) {
+  if (sprite.includes(pixel)) {
     crt[crtRow].push("#");
   } else {
     crt[crtRow].push(".");
   }
-  cycle2++;
+  pixel++;
 }
 
+// loop through data and execute the commands for both part 1 and 2
 for (let i = 0; i < data.length; i++) {
+  data[i] = data[i].split(" ");
+  data[i][1] = parseInt(data[i][1]);
+
   if (data[i][0] === "noop") {
     draw();
+    CalSigStr()
   } else if (data[i][0] === "addx") {
     draw();
+    CalSigStr()
     draw();
-    x2 += data[i][1];
-    sprite = [x2 - 1 , x2, x2 + 1]; // update sprite position according to the value of x
-    
+    CalSigStr()
+    x += data[i][1];
+    sprite = [x - 1 , x, x + 1]; // update sprite position according to the value of x
   }
 }
 
+// answer 1 is the sum of the signal strengths at the given cycles
+let sum = signalStrength.reduce((a, b) => a + b, 0);
+console.log("Part 1: " + sum);
+
+// answer 2 is the message drawn to the crt
 // merge each row into strings so they can be printed neatly to see the message
 let result = crt.map(row => row.join(""));
 console.log("Part 2:");
